584b29f7a5ef38b39d04ee964af224848349cb42
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-19 10:00:33 -0500

    PlacesViewController completed

--- Places/Model/PlacesModel.swift
+++ Places/Model/PlacesModel.swift
@@ -14,7 +14,25 @@ import MapKit
 // Connect to IB you need to have a dessendent of NSObject
 // When you create an instance in IB it must dessent NSObject
 
-class PlacesModel: NSObject {
+class PlacesModel: NSObject, UITableViewDataSource, UITableViewDelegate {
+    
     var places: [Location] = []
     let geocoder = CLGeocoder() // It takes a name/description and turn it a location with coordinates
+    
+    weak var mapViewController: MapViewController?
+    weak var tableView: UITableView?
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return places.count
+    }
+    
+    let cellReuseIdentifier = "Cell"
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        cell.textLabel?.text = places[indexPath.row].name
+        return cell
+    }
+    
+    
 }
--- Places/View Controller/PlacesViewController.swift
+++ Places/View Controller/PlacesViewController.swift
@@ -13,9 +13,34 @@ class PlacesViewController: UIViewController {
     
     let tableViewControllerSegueIdentifier = "TableViewControllerSegue"
     let mapviewControllerSegueIdentifier = "MapViewControllerSegue"
-    let cellReuseIdentifier = "Cell"
 
     @IBOutlet weak var placesModel: PlacesModel!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier else { return }
+        
+        if identifier == tableViewControllerSegueIdentifier,
+            let destination = segue.destination as? UITableViewController {
+            placesModel.tableView = destination.tableView
+            placesModel.tableView?.dataSource = placeModel
+            placesModel.tableView?.delegate = placesModel
+        }
+        
+        if identifier == mapviewControllerSegueIdentifier,
+            let destination = segue.destination as? MapViewController {
+            placesModel.mapViewController = destination
+        }
+    }
+    
+    @IBOutlet weak var field: UITextField!
+    @IBAction func lookuoLocation(_ sender: Any) {
+        guard let name = field.text, !name.isEmpty else { return }
+        
+        // Reset text
+        self.field.text = ""
+        placesModel.lookup(location: name)
+    }
+    
     
 }
 
